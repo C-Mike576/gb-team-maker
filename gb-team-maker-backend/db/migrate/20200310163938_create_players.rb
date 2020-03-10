3ab117b41b73e5f09125b8f570e2e520cf3c57bf
C-Mike576 <m.calkins576@gmail.com>
2020-03-10 12:42:02 -0400

    adding players to backend

---
+++ gb-team-maker-backend/db/migrate/20200310163938_create_players.rb
@@ -0,0 +1,13 @@
+class CreatePlayers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.string :team
+      t.string :position
+      t.string :card_front
+      t.string :card_back
+
+      t.timestamps
+    end
+  end
+end
