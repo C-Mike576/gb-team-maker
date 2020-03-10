f3ccbb506a5136e252198ea27fbd570a9356276b
C-Mike576 <m.calkins576@gmail.com>
2020-03-10 15:45:38 -0400

    set up seed data

---
+++ gb-team-maker-backend/db/seeds.rb
@@ -0,0 +1,105 @@
+Player.create(
+    name: "Shark", 
+    team: "Fishermen", 
+    position: "Captian", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583863954/sharkfront_umbtsp.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866431/sharkback_ipunj5.png"
+)
+Player.create(
+    name: "Corsair", 
+    team: "Fishermen", 
+    position: "Captian", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866440/corsairfront_h64ebs.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866438/corsairback_uqho63.png"
+)
+Player.create(
+    name: "Yukai", 
+    team: "Fishermen", 
+    position: "Captian", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866439/yukaifront_uojddu.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866437/yukaiback_tlmki0.png"
+)
+Player.create(
+    name: "Salt", 
+    team: "Fishermen", 
+    position: "Mascot", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866430/saltfront_mtjt0g.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866429/saltback_zkuo9g.png"
+)
+Player.create(
+    name: "Tentacles", 
+    team: "Fishermen", 
+    position: "Mascot", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866439/tentaclesfront_zchyqi.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866436/tentaclesback_ujenou.png"
+)
+Player.create(
+    name: "Angel", 
+    team: "Fishermen", 
+    position: "Squaddie", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866438/angelfront_ipeutr.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866438/angelback_v3bjjn.png"
+)
+Player.create(
+    name: "Grayscales", 
+    team: "Fishermen", 
+    position: "Squaddie", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866441/grayscalesfront_mkw8iq.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866442/grayscalesback_mjnuti.png"
+)
+Player.create(
+    name: "Hag", 
+    team: "Fishermen", 
+    position: "Squaddie", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866442/hagfront_qhms2r.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866441/hagback_bb11yh.png"
+)
+Player.create(
+    name: "Jac", 
+    team: "Fishermen", 
+    position: "Squaddie", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866428/jacfront_vtxmue.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866442/jacback_se9boc.png"
+)
+Player.create(
+    name: "Knucles", 
+    team: "Fishermen", 
+    position: "Squaddie", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866428/knucklesfront_tc9ilt.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866428/knucklesback_rrqhwf.png"
+)
+Player.create(
+    name: "Kraken", 
+    team: "Fishermen", 
+    position: "Squaddie", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866430/krakenfront_kx1wzs.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866431/krakenback_gbnj4k.png"
+)
+Player.create(
+    name: "Sakana", 
+    team: "Fishermen", 
+    position: "Squaddie", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866435/skanafront_foxcky.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866434/skanaback_mnpqcs.png"
+)
+Player.create(
+    name: "Sakana(v)", 
+    team: "Fishermen", 
+    position: "Squaddie", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866435/skana_v_front_uq61xu.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866434/skana_v_back_hjcgnx.png"
+)
+Player.create(
+    name: "Siren", 
+    team: "Fishermen", 
+    position: "Squaddie", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866435/sirenfront_oa4rwo.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866434/sirenback_nxoikw.png"
+)
+Player.create(
+    name: "Siren(v)", 
+    team: "Fishermen", 
+    position: "Squaddie", 
+    card_front: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866433/siren_v_front_brc7ky.png", 
+    card_back: "https://res.cloudinary.com/hwz50kv8c/image/upload/v1583866432/siren_v_back_ana58k.png"
+)
